0f35b30febb3fc537be84eb97ebf5c357bdbb3c4
KLusvarghi <kauaolusvarghi@gmail.com>
2025-04-10 21:51:51 -0300

    feat(routes): criação da rota do barrel que exporta todas as rotas de todas entidades

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const pessoas = require('./pessoasRoutes.js')
+// const matriculaa = require('./matriculasRoutes.js')
+// const cursos = require('./cursosRoutes.js')
+// const categorias = require('./categoriasRoutes.js')
+
+// O App é uma estancia do express, ele é responsavel por criar o servidor e configurar as rotas, e entre outras coisas.
+module.exports = app => {
+  // ele recebe o middeware para lidar com JSON, e todas as rotas de Pessoa, Matricula, Curso e Categoria
+  app.use(
+    express.json(),
+    pessoas,
+    // matriculaa,
+    // cursos,
+    // categorias
+  )
+}
+
+
