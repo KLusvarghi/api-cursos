25197426afde8903a74dd11b0eade067bc58fed7
KLusvarghi <kauaolusvarghi@gmail.com>
2025-04-16 21:41:06 -0300

    reaf(router): modificaçao na hora de chamar o metodo de pegar todos os users

--- src/routes/pessoasRoutes.js
+++ src/routes/pessoasRoutes.js
@@ -1,8 +1,13 @@
-const { Router } = require('express')
-const PessoasController = require('../controllers/PessoasController.js')
+const { Router } = require('express');
+const PessoaController = require('../controllers/PessoaController.js');
+
+// agora que os metodos não são mais statics, temos que estanciar o objeto
+const pessoaController = new PessoaController();
 
 const router = Router()
-router.get('/pessoas', PessoasController.getAll)
+// um dos motivos do cpodigo abaixo é poque agora estamos lidando com classes, e não mais com metodos estaticos
+// router.get('/pessoas', pessoaController.getAll)
+router.get('/pessoas', (req, res) => pessoaController.getAll(req, res))
 
 
 module.exports = router
