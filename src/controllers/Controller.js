1cec64f2b3391d333f0242ecca625cb6584762bc
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-02 22:19:45 -0300

    feat(controller): add new func

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -15,6 +15,55 @@ class Controller {
       // tratar error
     }
   }
+
+
+  async getById(req, res) {
+    const { id } = req.params;
+    try {
+      const register = await this.entidadeService.getResgisterById(Number(id));
+      return res.status(200).json(register);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async createNew(req, res) {
+    const dataToCreate = req.body;
+    try {
+      const newCreateRegister = await this.entidadeService.createRegister(dataToCreate);
+      return res.status(200).json(newCreateRegister);
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  // https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#simple-update-queries
+  async update(req, res) {
+    const { id } = req.params; // pega o id dos parametros
+    const updateData = req.body; //pegando os dados atualizado do body
+    try {
+      const isUpdate = await this.entidadeService.updateRegister(updateData, Number(id));
+      if (!isUpdate) {
+        return res.status(400).json({ mensagem: 'registro não foi atualizado' });
+      }
+      return res.status(200).json({ mensagem: 'Atualizado com sucesso' });
+    } catch (erro) {
+      // erro
+    }
+  }
+
+  async remove(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.removeRegister(Number(id));
+      return res.status(200).json({ mensagem: `id ${id} deletado` });
+
+
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
 }
 
 module.exports = Controller
