35988cac7ce3f1605f5c8277c503b84e88684644
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-05 22:49:17 -0300

    feat(service) add new function to get metricula by student id

--- src/Services/PessoaServices.js
+++ src/Services/PessoaServices.js
@@ -4,6 +4,12 @@ class PessoaServices extends Services {
   constructor() {
     super('Pessoa');
   }
+
+  async getMatriculasByStudentId(id) {
+    const student = await super.getResgisterById(id)
+    const matriculasList = await student.getAulasMatriculadas()
+    return matriculasList
+  }
 }
 
 module.exports = PessoaServices;
