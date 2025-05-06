7f44b071ea761e81434ce3de6aacef35a726a37a
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-05 22:44:52 -0300

    feat(associates) add associates of tables

--- src/models/pessoa.js
+++ src/models/pessoa.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // MOdelo é a camada que faz a representação dos dados na API, assim mapeando os dados relacionais que estão na tabela "Pessoa" (neste caso)
-  // Alem disso, a camada de modelo é responsavel por lidar com os dados, especificando como buscamos um dado, e todo o crud em geral, é resposabilidade em fazer a interface com a base de dados, além de ter as regras de negócio
+// Alem disso, a camada de modelo é responsavel por lidar com os dados, especificando como buscamos um dado, e todo o crud em geral, é resposabilidade em fazer a interface com a base de dados, além de ter as regras de negócio
 
 
 const {
@@ -9,13 +9,18 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      // passamos dois parametros, o primeiros é o modelo que estamos associando (está na outra ponta), e o segundo são propriedades extras, uma delas é a FK que é uma propriedade obrigatória
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: 'docente_id',
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: 'estudante_id',
+        // o scope é uma propriedade que nos permite fazer uma busca especifica, no caso, estamos buscando apenas os matriculados, sendo o 'status' uma propriedade de dentro da tabela 'matriculas', e o 'metriculado' o valor que queremos buscar
+        // scope: {status: 'metriculado'},
+        // o as um chave que usamos para passar apelidos para dentro das nossas tabelas
+        as: 'aulasMatriculadas'
+      })
     }
   }
   Pessoa.init({
@@ -29,7 +34,7 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'Pessoa',
     // para previnir futuros problemas com a questão do plural e singular, adicionamos o seguinte:
     tableName: 'pessoas', // com letra minuscula e no plural, assim ficando mais facil dele saber aonde chama o nome dos "modelos" e as "tabelas" que estão relacionadas a esses modelos
-      //  além de que, por padrão o sequelize plurariza o nome das tabelas, porém ele só sabe fazer isso para o ingles, então se você estiver usando o sequelize em outro idioma, pode ser que ele não pluralize corretamente
+    //  além de que, por padrão o sequelize plurariza o nome das tabelas, porém ele só sabe fazer isso para o ingles, então se você estiver usando o sequelize em outro idioma, pode ser que ele não pluralize corretamente
   });
   return Pessoa;
 };
