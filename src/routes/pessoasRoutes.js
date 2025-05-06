4baf15a5b354c8457aab0a8169046023b2ee5506
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-05 22:47:32 -0300

    feat(router) add new routers

--- src/routes/pessoasRoutes.js
+++ src/routes/pessoasRoutes.js
@@ -1,16 +1,20 @@
 const { Router } = require('express');
 const PessoaController = require('../controllers/PessoaController.js');
+const MatriculaController = require('../controllers/MatriculaController.js');
 
 // agora que os metodos não são mais statics, temos que estanciar o objeto
 const pessoaController = new PessoaController();
+const matriculaController = new MatriculaController();
 
 const router = Router()
 // um dos motivos do cpodigo abaixo é poque agora estamos lidando com classes, e não mais com metodos estaticos
 // router.get('/pessoas', pessoaController.getAll)
 router.get('/pessoas', (req, res) => pessoaController.getAll(req, res))
-// router.get('/pessoas/:id', (req, res) => pessoaController.pegaUmPorId(req, res));
-// router.post('/pessoas', (req, res) => pessoaController.criaNovo(req, res));
+router.get('/pessoas/:id', (req, res) => pessoaController.getById(req, res));
+router.post('/pessoas', (req, res) => pessoaController.createNew(req, res));
 router.put('/pessoas/:id', (req, res) => pessoaController.update(req, res));
-// router.delete('/pessoas/:id', (req, res) => pessoaController.exclui(req, res));
+router.delete('/pessoas/:id', (req, res) => pessoaController.remove(req, res));
+router.get('/pessoas/:estudanteId/matriculas', (req, res) => pessoaController.getMatriculas(req, res));
+router.post('/pessoas/:estudanteId/matriculas', (req, res) => matriculaController.createNew(req, res));
 
 module.exports = router
