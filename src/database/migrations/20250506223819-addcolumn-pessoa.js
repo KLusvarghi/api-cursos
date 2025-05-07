36ca35c312817e073ad62614f30410a525bda829
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-07 18:24:41 -0300

    ref(migration) add new column od deletedAt to all tables

---
+++ src/database/migrations/20250506223819-addcolumn-pessoa.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Esse é um arquivo de migração, e migração em ORM é basicamente alteraçẽos incrementais e raastreáveis no banco
+//  Um dos usos da migration é a "cordenação", poder cordenar alterações feitas nas tabelas do banco
+// sendo possivel rasterar as alterações feitas no banco de dados, para que seja possivel voltar para uma versão anterior
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    // Adicionando mais uma coluna na tabela 'pessoas', e para isso usamos o metodo **addColumn**
+    await queryInterface.addColumn('pessoas', 'deletedAt', {
+      allowNull: true, // se essa coluna pode ser nula ou não (tendo que passar true porque por padrão ela é null)
+      type: Sequelize.DATE // passando o tipo do dado
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    // Ele apenas chama o metodo **removeCOlumn**, caso seja necessario remover uma coluna
+    await queryInterface.removeColumn('pessoas', 'deletedAt')
+  }
+};
