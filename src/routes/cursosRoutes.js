f6b145a3e63a30678b891e3ffda14d01d6fe89b1
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-08 19:12:53 -0300

    ref(router) change the name of method

--- src/routes/cursosRoutes.js
+++ src/routes/cursosRoutes.js
@@ -5,7 +5,8 @@ const cursoController = new CursoController();
 
 const router = Router();
 
-router.get('/cursos', (req, res) => cursoController.getAll(req, res));
+// router.get('/cursos', (req, res) => cursoController.getAll(req, res));
+router.get('/cursos', (req, res) => cursoController.getCursos(req, res));
 router.get('/cursos/:id', (req, res) => cursoController.getById(req, res));
 router.post('/cursos', (req, res) => cursoController.createNew(req, res));
 router.put('/cursos/:id', (req, res) => cursoController.update(req, res));
