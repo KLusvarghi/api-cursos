012972f1b5e28e9017ea2076676e2d7eb5c45ccc
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-08 19:11:32 -0300

    ref(services) ref of func on pessoas services to be more generic

--- src/Services/PessoaServices.js
+++ src/Services/PessoaServices.js
@@ -5,14 +5,22 @@ class PessoaServices extends Services {
     super('Pessoa');
   }
 
-  async getMatriculasByStudentId(id) {
-    const student = await super.getResgisterById(id)
-    // const student = await this.getResgisterById(id)
+  async getActiveMatriculasByStudentId(id) {
+    // o super faz com que ele pegue as funções que estão na classe Services
+    const student = await super.getRegistersById(id)
+    // sendo o "aulasMatriculadas" o alias que criamos lá no model de "pessoa.js"
     const matriculasList = await student.getAulasMatriculadas()
     return matriculasList
   }
 
-  async getAllPeopleByScope(scope){
+  async getAllMatriculasByStudentId(id) {
+    const student = await super.getRegistersById(id)
+    // sendo o "allMatriculas" o alias que criamos lá no model de "pessoa.js"
+    const matriculasList = await student.getAllMatriculas()
+    return matriculasList
+  }
+
+  async getAllPeopleByScope(scope) {
     return await super.getRegistersByScope(scope)
   }
 }
